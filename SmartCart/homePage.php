b8d5072ce3375fce1e3074d196779e40a6532561
ROBXPERT <roberbahous@gmail.com>
2024-05-22 23:20:58 +0300

    Add files via upload

---
+++ SmartCart/homePage.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SmartCart</title>
+    <link rel="stylesheet" href="homePage.css">
+</head>
+<body>
+    <div class="video-container">
+        <video autoplay muted class="background-video">
+            <source src="images/donVid.mp4" type="video/mp4">
+        </video>
+    </div>
+</body>
+</html>
